178801dd9f92775834cec06ebe6f54e22f22bacc
Karel Pacheco Ramírez <54755265+skykarel@users.noreply.github.com>
2022-06-09 19:45:06 -0500

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -1,3 +1,11 @@
+"""
+Conexión hacía un servicio SQL en localhost
+Creado por:
+  Mejia Mondragón Cristian Alexis
+  Pacheco Ramírez Shai Karel
+  
+"""
+#Setup de la conexión a local
 import pymysql
 
 conexion = pymysql.connect(
@@ -9,8 +17,21 @@ conexion = pymysql.connect(
 
 cursor = conexion.cursor()
 
+#Formato de los creates
 SQL = "INSERT INTO users(nombre, apellido, correo) VALUES('karel', 'pacheco', 'karelpacheco36@aragon.unam.mx')"
-SQL = "INSERT INTO users(nombre, apellido, correo) VALUES('cristian', 'mejia', 'christianmejia10@aragon.unam.mx')"
 cursor.execute(sql)
+SQL = "INSERT INTO users(nombre, apellido, correo) VALUES('cristian', 'mejia', 'christianmejia@aragon.unam.mx')"
+cursor.execute(sql)
+
+
+#Formato del update
+SQL = "UPDATE SET correo = 'christianmejia10@aragon.unam.mx' WHERE (nombre ='cristian' AND apellido = 'mejia')
+cursor.execute(sql)
+
+
+#Formato de los delete
+SQL = "DELETE FROM users WHERE (nombre ='cristian' AND apellido = 'mejia')
+cursor.execute(sql)
+
 
 connection.commit()
