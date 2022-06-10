199dc3133f4bb0e5aa4785eca440cc4efd891360
requiem59 <94019721+requiem59@users.noreply.github.com>
2022-06-09 20:47:30 -0500

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -1,19 +1,21 @@
 import pymysql
 
-try:
+try:                                    #Se crea la conexión a la base de datos local dentro de un try para confirmar la conexión.
   conexion = pymysql.connect(
     host = 'localhost',
     user = 'root',
     password = 'aquelarre'
     db = 'biblioteca'
   )
-  print("\n-- Conexion correcta.")
+  print("\n-- Conexion correcta.")      
  except:
   print("\n-- Fallo en la conexión.")
 
-cursor = conexion.cursor()
+cursor = conexion.cursor()              #Se crea un cursor para manejar las entradas y salidas de la base de datos.
 
-sql = "SELECT * FROM libros;"
+sql = "SELECT * FROM libros;"           #Se crea una variable que contenga el query a ejecutar en la base de datos.
+
+#Ahora se intenta ejecutar el query dentro de un try para capturar errores en caso de que los haya.
 
 try:
     cursor.execute(sql)
@@ -24,4 +26,4 @@ try:
 except:
     print("\n-- Fallo en el query.")
 
-conexion.commit()
+conexion.commit()             #Por ultimo se guardan los cambios en la base.
