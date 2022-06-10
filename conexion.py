3541b3ae11ffb0877999fadf41018604947cf697
requiem59 <94019721+requiem59@users.noreply.github.com>
2022-06-09 20:41:57 -0500

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -12,3 +12,16 @@ try:
   print("\n-- Fallo en la conexión.")
 
 cursor = conexion.cursor()
+
+sql = "SELECT * FROM libros;"
+
+try:
+    cursor.execute(sql)
+    print("\n-- Query ejecutado correctamente.\n")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
+except:
+    print("\n-- Fallo en el query.")
+
+conexion.commit()
