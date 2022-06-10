2cec8290950fd70f162d2b1f5fa60a9ed9889810
requiem59 <94019721+requiem59@users.noreply.github.com>
2022-06-10 13:41:51 -0500

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -23,6 +23,15 @@ cursor = conexion.cursor()              #Se crea un cursor para manejar las entr
 
 sql = "SELECT * FROM libros;"           #Se crea una variable que contenga el query a ejecutar en la base de datos.
 
+#CREATE
+sql = "INSERT INTO libros(titulo, autor, año_publicacion, isbn, editorial) VALUES('Ustedes brillan en lo oscuro', 'Liliana Colanzi', 2022, ISBN , 'PÁGINAS DE ESPUMA');"
+
+#READ
+sql = "SELECT * FROM libros;"
+
+#DELETE
+sql = "DELETE FROM libros WHERE libro_id = 26"
+
 #Ahora se intenta ejecutar el query dentro de un try para capturar errores en caso de que los haya.
 
 try:
